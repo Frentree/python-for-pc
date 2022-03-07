341bbb0eaadd4bdb4b00268ee91071552dce6a8d
appcooker <appcooker@naver.com>
2022-03-07 18:28:38 +0900

    pre/post callbacks

--- process_protection/KMDFHelloWorld/Driver.c
+++ process_protection/KMDFHelloWorld/Driver.c
@@ -2,13 +2,19 @@
 #include <wdf.h>
 
 DRIVER_INITIALIZE DriverEntry;
-EVT_WDF_DRIVER_DEVICE_ADD KmdfHelloWorldEvtDeviceAdd;
 
-/*
-ObjectPreCallback(
+PVOID g_hRegistration = NULL;
+
+// NOTE : creating driver is unnecessary for developping.
+//EVT_WDF_DRIVER_DEVICE_ADD KmdfHelloWorldEvtDeviceAdd;
+
+OB_PREOP_CALLBACK_STATUS ObjectPreCallback(
 	_In_ PVOID RegistrationContext,
 	_In_ POB_PRE_OPERATION_INFORMATION OperationInformation)
 {
+	UNREFERENCED_PARAMETER(RegistrationContext);
+	UNREFERENCED_PARAMETER(OperationInformation);
+	/*
 	PREGCONTEXT RegContext = (PREGCONTEXT)RegistrationContext;
 
 	ULONG CurrentPid = (ULONG)PsGetProcessId((PERPROCESS)(OperationInformation->Object));
@@ -20,12 +26,16 @@ ObjectPreCallback(
 			OperationInformation->Parameters->CreateHandleInformation.DesiredAccess &= ~PROCESS_TERMINATE;
 		}
 	}
+	*/
 	return OB_PREOP_SUCCESS;
 }
 VOID ObjectPostCallback(
 	_In_ PVOID RegistrationContext,
-	_In_ POB_PRE_OPERATION_INFORMATION OperationInformation)
+	_In_ POB_POST_OPERATION_INFORMATION OperationInformation)
 {
+	UNREFERENCED_PARAMETER(RegistrationContext);
+	UNREFERENCED_PARAMETER(OperationInformation);
+	/*
 	PERPROCESS pEprocess;
 	PLIST_ENTRY pListEntry;
 
@@ -46,49 +56,50 @@ VOID ObjectPostCallback(
 		pListEntry->Flink = NULL;
 		pListEntry->Blink = NULL;
 	}
+	*/
 }
-*/
-void foo()
+NTSTATUS ObReg()
 {
-	OB_OPERATION_REGISTRATION oor = { 0, };
-	OB_CALLBACK_REGISTRATION ocr = { 0, };
+	OB_OPERATION_REGISTRATION opRegistration = { 0, };
+	OB_CALLBACK_REGISTRATION obRegistration = { 0, };
 
-	oor.ObjectType = PsProcessType;
-	oor.Operations = OB_OPERATION_HANDLE_CREATE | OB_OPERATION_HANDLE_DUPLICATE;
-	oor.PreOperation = ObjectPreCallback;
-	oor.PostOperation = ObjectPostCallback;
+	opRegistration.ObjectType = PsProcessType;
+	opRegistration.Operations = OB_OPERATION_HANDLE_CREATE;// | OB_OPERATION_HANDLE_DUPLICATE;
+	opRegistration.PreOperation = ObjectPreCallback;
+	opRegistration.PostOperation = ObjectPostCallback;
 
-	RtlInitUnicodeString(&ocr.Altitude, _T("33333"));
-	g_RegContext.ulMyPid = 55555;
-	g_RegContext.ulProtectPid = 55555;
+	//obRegistration.Altitude = usAltitude;
+	RtlInitUnicodeString(&obRegistration.Altitude, L"33333");
 
-	//ocr.Version = ObGetFilterVersion();
-	ocr.Version = OB_FLT_REGISTRATION_VERSION;
-	ocr.OperationRegistrationCount = 1;
-	ocr.Altitude = usAltitude;
-	ocr.RegistrationContext = (PVOID)&g_RegContext;
-	ocr.OperationRegistration = &oor;
+	obRegistration.Version = ObGetFilterVersion();
+	obRegistration.OperationRegistrationCount = 1;
+	obRegistration.RegistrationContext = NULL;//(PVOID)&g_RegContext;
+	obRegistration.OperationRegistration = &opRegistration;
 
-	ntStatus = ObRegisterCallbacks(&ocr, &g_RegHandle);
+	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "[F]: ObReg\n"));
+
+	return ObRegisterCallbacks(&obRegistration, &g_hRegistration);
 }
-
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 {
 	NTSTATUS status = STATUS_SUCCESS;
 
-	WDF_DRIVER_CONFIG config;
+	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "[F]: DriverEntry\n"));
 
-	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: DriverEntry\n"));
-
-	WDF_DRIVER_CONFIG_INIT(&config, KmdfHelloWorldEvtDeviceAdd);
-
-	memset(&config, 0, sizeof(config));
 	// Create the driver object
-	status = WdfDriverCreate(DriverObject, RegistryPath, WDF_NO_OBJECT_ATTRIBUTES, &config, WDF_NO_HANDLE);
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+	// NOTE : creating driver is unnecessary for developping.
+	//WDF_DRIVER_CONFIG config;
+	//memset(&config, 0, sizeof(config));
+	//WDF_DRIVER_CONFIG_INIT(&config, KmdfHelloWorldEvtDeviceAdd);
+	//status = WdfDriverCreate(DriverObject, RegistryPath, WDF_NO_OBJECT_ATTRIBUTES, &config, WDF_NO_HANDLE);
 
 	return status;
 }
 
+// NOTE : creating driver is unnecessary for developping.
+#if 0
 NTSTATUS KmdfHelloWorldEvtDeviceAdd(_In_ WDFDRIVER Driver, _Inout_ PWDFDEVICE_INIT DeviceInit)
 {
 	UNREFERENCED_PARAMETER(Driver);
@@ -104,3 +115,4 @@ NTSTATUS KmdfHelloWorldEvtDeviceAdd(_In_ WDFDRIVER Driver, _Inout_ PWDFDEVICE_IN
 
 	return status;
 }
+#endif
