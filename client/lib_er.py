833cbd6f637ab3a31956bcb0dd769b440146df3f
appcooker <appcooker@naver.com>
2022-04-18 10:27:44 +0900

    Update lib_er.py

--- client/lib_er.py
+++ client/lib_er.py
@@ -2,6 +2,7 @@ import urllib3
 import requests
 import json
 import sys
+import base64
 from datetime import datetime
 
 #URL = "https://192.168.0.18:8339/beta"
@@ -17,29 +18,39 @@ class er_agent():
     #URL = "https://192.168.0.18:8339/beta"
     URL = "https://192.168.56.102:8339/beta"
     userid = "admin"
-    userpw = "fren1212"
-    my_profile_label = "label1"
-    LOCATION_ROOT = ""
+    userpw_encoded = 'ZnJlbjEyMTI='
+    my_profile_label = "label1"# "frentree"
+    LOCATION_ROOT = "C:"
 
-    def __init__(self, er_host_addr):
+    def __init__(self, er_host_addr, log = None):
         self.DEBUG_ON = True
 
         self.URL = "https://"+er_host_addr+":8339/beta"
 
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         self.log = logclass()
+        if None != log:
+            self.log = log
         (self.my_group_id, self.my_target_id) = self.get_my_group_target_id()
-        self.debug("my group id:"+str(self.my_group_id))
-        self.debug("my target id:"+str(self.my_target_id))
+        self.log.info("my group id:"+str(self.my_group_id))
+        self.log.info("my target id:"+str(self.my_target_id))
         self.my_current_matches = self.my_summary_target_matches()
         self.my_datatype_profile_id = self.get_my_datatype_profile_id()
-        self.debug("my profile id:"+str(self.my_datatype_profile_id))
-        self.debug('matches:'+self.my_current_matches)
+        self.log.info("my profile id:"+str(self.my_datatype_profile_id))
+        self.log.info('matches:'+str(self.my_current_matches))
         #self.get_my_matchobjects()
+        if None == self.my_group_id:
+            print("default group id " + self.default_group_id)
+            self.create_server_target(self.my_hostname, self.default_group_id)
+        locations = self.my_list_locations()
+        # for location in locations:
+        #     print("#################### my location ################")
+        #     print(json.dumps(location, indent=4))
+        if 0 == len(locations):
+            self.location_add_local(self.my_target_id, "C:")
 
     def get_my_matchobjects(self):
         result = self.request('get', '/targets/'+self.my_target_id+'/matchobjects')
-        print(json.dumps(result, indent=4))
 
     def get_my_group_target_id(self):
         my_target_id = None
@@ -47,12 +58,14 @@ class er_agent():
 
         import platform
         self.my_hostname = platform.node()
-        self.my_hostname = 'DESKTOP-J6FK55A'      # NOTE: dev
+        #self.my_hostname = 'DESKTOP-J6FK55A'      # NOTE: dev
         # target name: DESKTOP-J6FK55A ==> vbox vm win, id: 14952095194870184286
         self.debug('hostname:'+self.my_hostname)
 
         result = self.list_groups()
         for group in result:
+            if 'DEFAULT GROUP' == group['name']:
+                self.default_group_id = group['id']
             if 'targets' not in group: continue
             for target in group['targets']:
                 if target['name'] == self.my_hostname:
@@ -73,14 +86,15 @@ class er_agent():
         req_url = self.URL + url
 
         print("URL:"+req_url)
+        userpw = base64.b64decode(self.userpw_encoded).decode('ascii')
         if 'post' == method:
             headers = {'Content-Type': 'application/json; charset=utf-8'}
-            res = requests.post(req_url, headers=headers, auth=(self.userid, self.userpw), verify=False, data = payload)
+            res = requests.post(req_url, headers=headers, auth=(self.userid, userpw), verify=False, data = payload)
         elif 'get' == method:
-            res = requests.get(req_url, auth=(self.userid, self.userpw), verify=False)
+            res = requests.get(req_url, auth=(self.userid, userpw), verify=False)
         elif 'delete' == method:
             headers = {'Content-Type': 'application/json'}
-            res = requests.delete(req_url, headers=headers, auth=(self.userid, self.userpw), verify=False)
+            res = requests.delete(req_url, headers=headers, auth=(self.userid, userpw), verify=False)
 
         try:
             ret = res.json()
@@ -200,6 +214,20 @@ class er_agent():
             url += '/' + str(schedule_id)
         return self.request('get', url)
 
+    def is_schedule_completed(self, schedule_id):
+        result = self.list_schedules(schedule_id)
+        self.prt("SCHEDULE", result)
+        if 'targets' not in result: return False
+        if len(result['targets']) < 1: return False
+        if 'locations' not in result['targets'][0]: return False
+        if len(result['targets'][0]['locations']) < 1: return False
+        if 'status' not in result['targets'][0]['locations'][0]: return False
+        if 'completed' != result['targets'][0]['locations'][0]['status']: return False
+
+        self.prt("targets", result['targets'])
+        self.prt("locations", result['targets'][0]['locations'][0]['status'])
+        return True
+
     # data structure of location list
     #   [
     #       {
@@ -218,6 +246,7 @@ class er_agent():
                 self.my_datatype_profile_id,         # frentree      TODO
             ],
         }
+        self.log.info(data)
         self.prt("ADD SCHEDULE DATA", data)
         ret = self.request('post', '/schedules', payload=json.dumps(data))
         return ret
@@ -229,9 +258,9 @@ class er_agent():
     def my_add_schedule(self, subpath_list):
         location_id = self.get_location_id_by_path(self.my_target_id, \
             self.LOCATION_ROOT) # TODO
-        self.debug('location_id:'+str(location_id))
+        self.log.info('location_id:'+str(location_id))
         new_label = self.my_hostname+"_"+datetime.now().strftime("%Y%m%d_%H%M%S")
-        self.debug('new label:' + new_label)
+        self.log.info('new label:' + new_label)
 
         location_list = []
 
@@ -241,6 +270,7 @@ class er_agent():
                 'subpath':subpath,
             })
         result = self.add_schedule(self.my_target_id, new_label, location_list)
+        print(json.dumps(result, indent=4))
         # NOTE: schedule id will be just one whether the param subpath is multiple or not
         # success example : {'id': '44'}
         if 'id' in result:
@@ -267,7 +297,15 @@ class er_agent():
     #endregion
 
 def main():
-    er = er_agent("192.168.56.102")
+    #a_list = [1,20,3,4,5]
+    #a_list = list(map(lambda x : print(x * 10) return x, a_list))
+    #print(a_list)
+    #sys.exit(0)
+
+    from lib_logging import config_logging, log
+    config_logging()
+
+    er = er_agent("192.168.12.7", log)
 
     #result = er.create_server_target("DESKTOP-J6FK55A", 10155220174825011556)
     #result = er.delete_location(12138559403110519359, 12893076805411359213)
@@ -278,12 +316,13 @@ def main():
     #print(json.dumps(result, indent=4))
 
     result = er.my_list_groups()
+    #print(json.dumps(result, indent=4))
     result = er.my_list_locations()
 
     from libsqlite3 import csqlite3
     workdir_path = "."
-    SQLITEDB_FILENAME = "prj1"#"state"
-    sqlite3 = csqlite3(workdir_path + '/' + SQLITEDB_FILENAME + ".db")
+    SQLITEDB_FILENAME = "state"
+    sqlite3 = csqlite3(workdir_path + '/dist/' + SQLITEDB_FILENAME + ".db")
     file_list = sqlite3.fileinfo_select()
 
     for fileinfo in file_list:
@@ -291,14 +330,28 @@ def main():
         file_path = fileinfo[1]
         file_size = fileinfo[2]
         file_state = fileinfo[3]
-        schedule_id = er.my_add_schedule(subpath_list=[
-            #file_path,
-            '\\users\\danny\\desktop\\s3.txt',
-        ])
-        print("schedule added " + str(schedule_id))
+        # schedule_id = er.my_add_schedule(subpath_list=[
+        #     file_path,
+        #     #'\\users\\danny\\desktop\\s3.txt',
+        # ])
+        # print("schedule added " + str(schedule_id))
 
-    result = er.list_schedules()
-    er.prt("list schedule", result)
+    # file_path = '\\Users\\Admin\\Desktop\\aaa\\8.txt'
+    # # file_path = 'C:\\Users\\danny\\Desktop\\s2.txt'
+    # schedule_id = er.my_add_schedule(subpath_list=[
+    #     file_path,
+    #     #'\\users\\danny\\desktop\\s3.txt',
+    # ])
+    schedule_id = 145
+    file_list = sqlite3.fileinfo_select_scheduled()
+    print(file_list)
+    #print("schedule added " + str(schedule_id))
+
+    #result = er.list_schedules(schedule_id)
+    #er.prt("list schedule", result)
+
+    #er.is_schedule_completed(schedule_id)
+    sys.exit(0)
     #for schedule_id in range(86, 100):
     #    er.update_schedule(schedule_id, 'deactivate')
     sys.exit(0)
@@ -341,4 +394,9 @@ def main():
     # do_er("/licenses", formatted=True)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        import traceback
+        print(traceback.print_stack())
+        print(e)        
