d6a4f7773a4fb7a262bb51c72115f20ca760cd99
appcooker <appcooker@naver.com>
2022-04-22 08:00:04 +0900

    api interface

--- client/lib_apiinterface.py
+++ client/lib_apiinterface.py
@@ -9,6 +9,24 @@ class cApiInterface:
     self.log = log
     self.hostname = cwinsecurity.get_hostname()
 
+  def c2s_jobPost(self, post_data):
+    url = 'http://'+self.server_address+'/c2s_job/' + self.hostname
+
+    self.log.debug(url)
+    r = requests.post(url, json=post_data)
+    ret = r.json()
+    self.log.debug(json.dumps(ret, indent=4))
+    return ret
+
+  def c2s_jobGet(self):
+    url = 'http://'+self.server_address+'/c2s_job/' + self.hostname
+
+    self.log.debug(url)
+    r = requests.get(url)
+    ret = r.json()
+    self.log.debug(json.dumps(ret, indent=4))
+    return ret
+
   def drm_configGet(self):
     url = 'http://'+self.server_address+'/drm_config'
 
@@ -21,7 +39,7 @@ class cApiInterface:
   def v_drm_scheduleGet(self):
     url = 'http://'+self.server_address+'/v_drm_schedule' + "/" + self.hostname
 
-    self.log.info(url)
+    self.log.debug(url)
     r = requests.get(url)
     ret = r.json()
     self.log.debug(json.dumps(ret, indent=4))
@@ -34,7 +52,12 @@ class cApiInterface:
     ret = ret['result'][0]
     return ret
 
-  def pi_schedulesPost(self):
+  def pi_schedulesPost(self, schedule_id, ap_no, DRM_STATUS):
     url = 'http://'+self.server_address+'/pi_schedules' + "/" + self.hostname
-    r = requests.post(url)
-    self.log.info("################### pi_schedulesPost")
+    post_data = {
+      'schedule_id': schedule_id,
+      'ap_no': ap_no,
+      'DRM_STATUS' : DRM_STATUS,
+    }
+    r = requests.post(url, json=post_data)
+    self.log.info("################### pi_schedulesPost " + str(DRM_STATUS))
