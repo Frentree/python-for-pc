0951b93fde2f469df07fe9fe692c355b4e5cad3a
appcooker <appcooker@naver.com>
2022-04-27 16:55:19 +0900

    build.bat

--- client/build.bat
+++ client/build.bat
@@ -1,5 +1,7 @@
 rem start /b kill.bat
 
+taskkill /F /IM installer.exe
+
 cd dist
 installer remove
 cd ..
@@ -13,13 +15,18 @@ rem timeout 1
 rem taskkill /F /IM x64.exe
 rem timeout 1
 
-python -m PyInstaller -F --hidden-import=win32timezone -n installer main.py
+taskkill /F /IM installer.exe
+python -m PyInstaller -F --hidden-import=win32timezone -n ftclient main.py
 cd dist
 installer
 cd ..
 
+dist\installer remove
+dist\installer install
+dist\installer start
 
-copy dist\ftclient.exe ..\installer\ftservice
+copy dist\ftclient.exe ..\00.RELEASE\package.exe
+python lib_pysftp.py
 
 rem c:\windows\system32\msiexec /x ftservice.msi
 rem cd ..\installer & devenv /ReBuild Release ftservice.sln
