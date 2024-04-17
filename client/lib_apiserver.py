2ee72b455edde351e81bc8fa3f9dde5ccc88c086
jaeha\klevra <sadviolent@gmail.com>
2024-04-17 16:10:10 +0900

    adding mac address(from agent.cfg) to domain data 240417 by hjh

--- client/lib_apiserver.py
+++ client/lib_apiserver.py
@@ -1,17 +1,18 @@
 import requests
 import json
 import os
-import uuid
+# import uuid # DRM 영역 로컬PC 호스트명 중복으로 인한 mac address 추가작업, 불필요한 소스로 주석처리
+# 2403XX agent.cfg 파일을 읽기위한 헤더 by 김주원
+import xml.etree.ElementTree as ET
 
 class cApiServer:
   def __init__(self, server_addr, server_port, hostname, log):
     self.server_addr = server_addr
     self.server_port = server_port
     #self.hostname = hostname
-    self.hostname = get_agent_mac()
+    self.hostname = get_mac_domain()
     self.log = log
-    # self.hostname2 = get_agent_mac()
-
+    
   def c2s_jobPost(self, post_data):
     url = 'http://'+self.server_addr+':'+self.server_port+'/c2s_job/' + self.hostname
 
@@ -140,35 +141,44 @@ class cApiServer:
         os.system(cmd)
 
 
-def get_agent_mac():    # 맥 에이전트 명을 가져오기 위한 함수
-    temp_file = os.path.expandvars(f'%temp%\\output_{uuid.uuid4().hex}.txt')
-    
-    try:
-        os.system(f'ipconfig /all > {temp_file}')
+# def get_agent_mac():    # 맥 에이전트 명을 가져오기 위한 함수
+#     temp_file = os.path.expandvars(f'%temp%\\output_{uuid.uuid4().hex}.txt')
+#     try:
+#         os.system(f'ipconfig /all > {temp_file}')
+#         with open(temp_file, 'r', encoding='CP949') as f:
+#             content = f.read()
+#             interfaces = content.split("\n\n")  # 이더넷 어댑터별로 분리
+#             connected_macs = []
+#             for interface in interfaces:
+#                 if "물리적 주소" in interface:
+#                     for line in interface.split("\n"):
+#                         if "물리적 주소" in line:
+#                             mac = line.split(":")[1].strip().replace('-', '').upper()
+#                             connected_macs.append(mac)
+#                             break
+#         if connected_macs:
+#             return os.getenv("COMPUTERNAME", "") #+ "." + connected_macs[0]
+#             # return os.getenv("COMPUTERNAME", "") + "." + connected_macs[0]
+#         else:
+#             return None
+#     finally:
+#         # 항상 임시 파일 삭제 (예외가 발생하더라도)
+#         try:
+#             os.remove(temp_file)
+#         except Exception as e:
+#             print(f"Error removing temporary file: {e}")
 
-        with open(temp_file, 'r', encoding='CP949') as f:
-            content = f.read()
+# 2403XX 주원님 공유된 맥주소 호출 by 김주원
+def get_mac_domain():
+  with open('C:/Program Files (x86)/Ground Labs/Enterprise Recon 2/agent.cfg','r') as cfg_file:
+    cfg_content = cfg_file.read()
+    root = ET.fromstring(cfg_content)
+    domain_element = root.find('./domain')
 
-            interfaces = content.split("\n\n")  # 이더넷 어댑터별로 분리
-            connected_macs = []
-
-            for interface in interfaces:
-                if "물리적 주소" in interface:
-                    for line in interface.split("\n"):
-                        if "물리적 주소" in line:
-                            mac = line.split(":")[1].strip().replace('-', '').upper()
-                            connected_macs.append(mac)
-                            break
-
-        if connected_macs:
-            return os.getenv("COMPUTERNAME", "") #+ "." + connected_macs[0]
-            # return os.getenv("COMPUTERNAME", "") + "." + connected_macs[0]
-        else:
-            return None
-
-    finally:
-        # 항상 임시 파일 삭제 (예외가 발생하더라도)
-        try:
-            os.remove(temp_file)
-        except Exception as e:
-            print(f"Error removing temporary file: {e}")
+    if domain_element is not None:
+      domain_value = domain_element.text
+      print("domain_value : " + domain_value)
+      return os.getenv("COMPUTERNAME", "") + "." + domain_value
+    else:
+      print("domain element is not found in ter XML structure.")
+      return os.getenv("COMPUTERNAME", "")
--- client/main.py
+++ client/main.py
@@ -27,7 +27,9 @@ import lib_patterns
 from watchdog.observers import Observer
 from lib_cpupriority import setpriority
 
-import uuid   # DRM 영역 로컬PC 호스트명 중복으로 인한 mac address 추가작업
+# import uuid   # DRM 영역 로컬PC 호스트명 중복으로 인한 mac address 추가작업, 불필요한 소스로 주석처리
+# 2403XX agent.cfg 파일을 읽기위한 헤더 by 김주원
+import xml.etree.ElementTree as ET
 
 # region development
 def dev_diag():
@@ -455,7 +457,7 @@ def proc_cmdline():
     apiServer = lib_apiserver.cApiServer(
       server_addr = _G['API_SERVER_ADDR'],
       server_port = _G['API_SERVER_PORT'],
-      hostname = get_agent_mac(),
+      hostname = get_mac_domain(),
       log = log)
     print(len(sys.argv))
     filepath = ""
@@ -564,7 +566,7 @@ def proc_cmdline():
     apiServer = lib_apiserver.cApiServer(
       server_addr = _G['API_SERVER_ADDR'],
       server_port = _G['API_SERVER_PORT'],
-      hostname = get_agent_mac(),
+      hostname = get_mac_domain(),
       log = log)
 
     # get config to connect to ER2
@@ -583,7 +585,7 @@ def proc_cmdline():
     apiServer = lib_apiserver.cApiServer(
       server_addr = _G['API_SERVER_ADDR'],
       server_port = _G['API_SERVER_PORT'],
-      hostname = get_agent_mac(),
+      hostname = get_mac_domain(),
       log = log)
 
     # get config to connect to ER2
@@ -782,7 +784,7 @@ def DO_proc_job(dscs_dll, cmd, sqlite3):
     apiServer = lib_apiserver.cApiServer(
       server_addr = _G['API_SERVER_ADDR'],
       server_port = _G['API_SERVER_PORT'],
-      hostname = get_agent_mac(),
+      hostname = get_mac_domain(),
       log = log)
     job_result_list = []
     job_result['success'] = True
@@ -795,7 +797,7 @@ def DO_proc_job(dscs_dll, cmd, sqlite3):
     apiServer = lib_apiserver.cApiServer(
       server_addr = _G['API_SERVER_ADDR'],
       server_port = _G['API_SERVER_PORT'],
-      hostname = get_agent_mac(),
+      hostname = get_mac_domain(),
       log = log)
     apiServer.c2s_logfile(cmd['path'])
     job_result['success'] = True
@@ -972,7 +974,7 @@ def start_watchdog():
   # endregion WATCHDOG
 
 def drm_main():
-  er = lib_er.er_agent(hostname = get_agent_mac(), log=log)
+  er = lib_er.er_agent(hostname = get_mac_domain(), log=log)
 
   log.info("############################ DRM Main Loop Start (pid: " + str(lib_process.get_self_pid()) + ")")
   prev_network_usage = None
@@ -994,7 +996,7 @@ def drm_main():
       apiServer = lib_apiserver.cApiServer(
         server_addr = _G['API_SERVER_ADDR'],
         server_port = _G['API_SERVER_PORT'],
-        hostname = get_agent_mac(),
+        hostname = get_mac_domain(),
         log = log)
 
       try:
@@ -1061,7 +1063,7 @@ def drm_main():
       hdd = psutil.disk_usage('/')
       disk_usage = str(int(hdd.free / 2**20)) + " MB free"
       post_data = {
-        'hostname'        : get_agent_mac(),
+        'hostname'        : get_mac_domain(),
         'total_cpu'       : str(lib_misc.lib_cpu_usage()) + "/" + str(psutil.cpu_count()),
         'total_disk'      : disk_usage,
 
@@ -1390,38 +1392,48 @@ def svcinit(svc_name, svc_display_name, svc_description):
   else:
     MyServiceFramework.svcinit(svc_name, svc_display_name, svc_description)
 
-def get_agent_mac():    # 맥 에이전트 명을 가져오기 위한 함수
-    temp_file = os.path.expandvars(f'%temp%\\output_{uuid.uuid4().hex}.txt')
-    
-    try:
-        os.system(f'ipconfig /all > {temp_file}')
+# def get_agent_mac():    # 맥 에이전트 명을 가져오기 위한 함수
+#     temp_file = os.path.expandvars(f'%temp%\\output_{uuid.uuid4().hex}.txt')
+#     try:
+#         os.system(f'ipconfig /all > {temp_file}')
+#         with open(temp_file, 'r', encoding='CP949') as f:
+#             content = f.read()
+#             interfaces = content.split("\n\n")  # 이더넷 어댑터별로 분리
+#             connected_macs = []
+#             for interface in interfaces:
+#                 if "물리적 주소" in interface:
+#                     for line in interface.split("\n"):
+#                         if "물리적 주소" in line:
+#                             mac = line.split(":")[1].strip().replace('-', '').upper()
+#                             connected_macs.append(mac)
+#                             break
+#         if connected_macs:
+#             return os.getenv("COMPUTERNAME", "") # + "." + connected_macs[0]
+#             # return os.getenv("COMPUTERNAME", "") + "." + connected_macs[0]
+#         else:
+#             return None
+#     finally:
+#         # 항상 임시 파일 삭제 (예외가 발생하더라도)
+#         try:
+#             os.remove(temp_file)
+#         except Exception as e:
+#             print(f"Error removing temporary file: {e}")
 
-        with open(temp_file, 'r', encoding='CP949') as f:
-            content = f.read()
 
-            interfaces = content.split("\n\n")  # 이더넷 어댑터별로 분리
-            connected_macs = []
+# 2403XX 주원님 공유된 맥주소 호출 by 김주원
+def get_mac_domain():
+  with open('C:/Program Files (x86)/Ground Labs/Enterprise Recon 2/agent.cfg','r') as cfg_file:
+    cfg_content = cfg_file.read()
+    root = ET.fromstring(cfg_content)
+    domain_element = root.find('./domain')
 
-            for interface in interfaces:
-                if "물리적 주소" in interface:
-                    for line in interface.split("\n"):
-                        if "물리적 주소" in line:
-                            mac = line.split(":")[1].strip().replace('-', '').upper()
-                            connected_macs.append(mac)
-                            break
-
-        if connected_macs:
-            return os.getenv("COMPUTERNAME", "") # + "." + connected_macs[0]
-            # return os.getenv("COMPUTERNAME", "") + "." + connected_macs[0]
-        else:
-            return None
-
-    finally:
-        # 항상 임시 파일 삭제 (예외가 발생하더라도)
-        try:
-            os.remove(temp_file)
-        except Exception as e:
-            print(f"Error removing temporary file: {e}")
+    if domain_element is not None:
+      domain_value = domain_element.text
+      print("domain_value : " + domain_value)
+      return os.getenv("COMPUTERNAME", "") + "." + domain_value
+    else:
+      print("domain element is not found in ter XML structure.")
+      return os.getenv("COMPUTERNAME", "")
 
 
 # endregion
