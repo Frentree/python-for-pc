346565397562856b35b1dd0f8b6996ed7694b6f9
jaeha\klevra <sadviolent@gmail.com>
2024-04-22 19:59:10 +0900

    build 관련 소스 주석 작성 완료

--- client/build.bat
+++ client/build.bat
@@ -1,10 +1,19 @@
+@REM note : 버전 정보가 들어가있는 파일 생성
 python build.py generate_file_version
 
+@REM note : 기존 생성되었던 구버전 삭제
 del dist\ftclient.exe
+
+@REM note : ftclient(실질적으로는 package.exe) 생성
 python -m PyInstaller --version-file file_version.txt -F --hidden-import=win32timezone -n ftclient main.py
+
+@REM note : 생성된 ftclient를 package로 이름 변경
 copy dist\ftclient.exe  ..\00.RELEASE\package.exe
 
+@REM note : 생성된 패키지 파일 사인 작업
 cmd /c winsign ..\00.RELEASE\package.exe
+
+@REM note : 생성된 파일들 압축 파일로 생성
 python build.py
 rem python tester.py
 
--- client/build.py
+++ client/build.py
@@ -4,25 +4,37 @@ import json
 import datetime
 import zipfile
 
+# note : 설치파일 압축하기 위한 소스
 def build_zip(dir_path, postfix, API_SERVER_ADDR, DSCS_ALL_FILES_TRAVERSED, add_runbat = False):
+  # note : 상위 경로 접근 후 기존 생성된 설치파일 압축파일 삭제 
   os.chdir(dir_path)
   os.system("del " + 'ftclient_installer_'+postfix+'.zip')
+
+  # note : 새로이 압축할 zip 파일 생성
   my_zip = zipfile.ZipFile('ftclient_installer_'+postfix+'.zip', 'w')
+
+  # note : 설정파일 세팅에 맞게 복사
   path_conf_json = "configuration_"+postfix+".json"
   os.system("copy configuration.json " + path_conf_json)
 
+  # note : 경로 및 설정파일 확인(경로 체크)
   print("dir_path : " + dir_path)
   print("path_conf_json : " + path_conf_json)
+
   with open(path_conf_json, 'r', encoding='utf-8-sig') as json_conf_file:
+    # note : 설정파일을 읽어 Dictionary로 저장
     conf_content = json_conf_file.read()
     conf_dic = json.loads(conf_content)
 
+  # note : 설정파일 내 기초적인 항목 추가(서버명, 전체 체크 여부)
   conf_dic['API_SERVER_ADDR']           = API_SERVER_ADDR
   conf_dic['DSCS_ALL_FILES_TRAVERSED']  = DSCS_ALL_FILES_TRAVERSED
 
+  # note : 작성된 dictionary 데이터를 설정 파일에 맞게 기입
   with open(path_conf_json, 'w', encoding='utf-8-sig') as json_conf_file:
     json.dump(conf_dic, json_conf_file, indent=4, ensure_ascii=False)
   
+  # note : 생성된 설정파일 및 실행, 설치 지원파일등을 같이 압축파일에 추가
   my_zip.write('configuration_'+postfix+'.json',  'configuration.json', compress_type = zipfile.ZIP_DEFLATED)
   my_zip.write('install.exe',                     'install.exe',        compress_type = zipfile.ZIP_DEFLATED)
   my_zip.write('uninstall.exe',                   'uninstall.exe',      compress_type = zipfile.ZIP_DEFLATED)
@@ -30,8 +42,10 @@ def build_zip(dir_path, postfix, API_SERVER_ADDR, DSCS_ALL_FILES_TRAVERSED, add_
   if add_runbat:
     my_zip.write('run.bat',                       'run.bat',            compress_type = zipfile.ZIP_DEFLATED)
 
+  # note : 압축된 파일을 return
   return os.getcwd() + os.sep + 'ftclient_installer_'+postfix+'.zip'
 
+# note : 최초개발자가 관리 및 확인하기 위한 파일 전송 부분, 계약 파기로 인해 불필요한 함수, 정리 필요
 def upload(filename_src, filename_dst):
   import pysftp
   import datetime
@@ -76,11 +90,15 @@ def upload(filename_src, filename_dst):
 
   print("Uploaded " + filename_dst)
 
+# note : 파일 버전 생성 함수, 파일 버전은 ftclient.exe가 빌드될 시점으로, yyyymmdd_hhmm 으로 생성됨
 def generate_file_version(file_version):
+  # note : 데이터 작성될 파일에 대한 버전 정보는 file_version.txt에 생성(버퍼 성향의 파일)
   run_filename = 'file_version.txt'
+  # note : 기존에 파일이 존재 시 삭제
   if os.path.exists(run_filename):
     os.remove(run_filename)
 
+  # note : 데이터 생성, 인코딩 타입은 UTF-8 타입으로 생성
   f = open(run_filename, 'a')  # open file in append mode
   file_content = '''# UTF-8
 #
@@ -121,23 +139,31 @@ StringFileInfo(
 VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
   ]
 )'''
+  # note : 버전 내용에 대한 데이터는 변수로 받아와 사용
   file_content = file_content.replace('___FILE_VERSION___', file_version)
-
+  # 파일 생성 후 파일 닫기
   f.write(file_content)
   f.close()
 
+# note : build.py 본문
 if __name__ == '__main__':
   #cmd = 'python -m PyInstaller -F --hidden-import=win32timezone -n ftclient main.py'
   #os.system(cmd)
+  
+  # note : 설치 파일등 작업 위치 지정
   dir_path = "..\\00.RELEASE"
+
+  # note : 버전정보 추출을 위한 데이터 정리
   datestr     = (datetime.datetime.now().strftime("%Y%m%d"))
   datetimestr = (datetime.datetime.now().strftime("%Y%m%d_%H%M"))
   print(datetimestr)
-
+  
+  # note : build.py에서 옵션값으로 값이 들어왔을 때 버전 파일을 생성하자
   if len(sys.argv) > 1 and sys.argv[1] == "generate_file_version":
     generate_file_version(datetimestr)
     sys.exit(0)
 
+  # note : 배포 파일 생성 시 배치 실행(설치 시 정상 작동 체크 하기 위해)
   os.chdir(dir_path)
   run_filename = 'run.bat'
   
@@ -151,11 +177,13 @@ if __name__ == '__main__':
   
   print(dir_path)
 
+  # note : 해당 설정에 따른 설치파일에 대한 압축파일 생성
   # build_zip(dir_path, datestr+"_"+"150.19.24.208_traversed",      "150.19.24.208", "True")
   build_zip(dir_path, datestr+"_"+"150.19.24.208_not_traversed",  "150.19.24.208", "False")
   # build_zip(dir_path, datestr+"_"+"183.107.9.230_traversed",      "183.107.9.230", "True")
   # build_zip(dir_path, datestr+"_"+"183.107.9.230_not_traversed",  "183.107.9.230", "False")
 
+  # note : 이하 최초개발자에게 파일을 업로드 하기 위한 소스
   # 183.107.9.230_not_traversed
   # installer_2b_uploaded = build_zip(dir_path, datestr,  "183.107.9.230", "False", True)
 
